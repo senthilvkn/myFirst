bc71257a2742e5724c00bfba40169e4a9ed86703
sentnara <sentnara@LIN22002516.corp.capgemini.com>
2020-04-12 15:29:00 +0530

    mavenprojtojenkins

---
+++ MavenProjforJenkins/src/main/java/MavenProjforJenkins/MavenProjforJenkins/App.java
@@ -0,0 +1,13 @@
+package MavenProjforJenkins.MavenProjforJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Rock!" );
+    }
+}
