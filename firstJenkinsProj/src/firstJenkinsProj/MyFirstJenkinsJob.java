a23607274da6a6d08e0cbd808aecb3a7cd2a83b1
sentnara <sentnara@LIN22002516.corp.capgemini.com>
2020-04-12 15:01:26 +0530

    first proj to run in jenkins

---
+++ firstJenkinsProj/src/firstJenkinsProj/MyFirstJenkinsJob.java
@@ -0,0 +1,10 @@
+package firstJenkinsProj;
+
+public class MyFirstJenkinsJob {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Rock star");
+	}
+
+}
